342ec98968eb3e56c32ed59d4c01a6cfb2de9e0b
Павел Ефимов <gangplank009@gmail.com>
2023-10-03 13:11:15 +0300

    Init commit

---
+++ src/main/java/com/example/tbdodstest/TbdOdsTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.tbdodstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbdOdsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbdOdsTestApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/example/tbdodstest/TbdOdsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tbdodstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbdOdsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
